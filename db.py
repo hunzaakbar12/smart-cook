2ea7ae67e532018f8f0b08b58eeb7762d64ccf65
Hunza Akbar <hunzaakbar12@gmail.com>
2025-11-12 13:22:47 +0100

    services.py aktualisiert

--- db.py
+++ db.py
@@ -1,17 +1,7 @@
 import sqlite3
-import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(BASE_DIR, "smart_cook_ultra_simple_BIG.db")
-
-connection = sqlite3.connect(db_path)
-cursor = connection.cursor()
-
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = cursor.fetchall()
-
-print("Tables names:")
-for t in tables:
-    print("-", t[0])
-
-connection.close()
+conn = sqlite3.connect("smart_cook_ultra_simple_BIG.db")
+cur = conn.cursor()
+cur.execute("SELECT name FROM recipes LIMIT 10;")
+print(cur.fetchall())
+conn.close()
