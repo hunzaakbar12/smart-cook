f2528ec9faa59dcb3b79590aa9fe3247045810b8
Hunza Akbar <hunzaakbar12@gmail.com>
2025-11-12 11:47:04 +0100

    Fix DB script (__file__ bug)

--- db.py
+++ db.py
@@ -1,19 +1,16 @@
 import sqlite3
 import os
 
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "smart_cook_ultra_simple_BIG.db")
 
- 
 connection = sqlite3.connect(db_path)
 cursor = connection.cursor()
-
 
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 tables = cursor.fetchall()
 
-print("Tables names: ")
+print("Tables names:")
 for t in tables:
     print("-", t[0])
 
